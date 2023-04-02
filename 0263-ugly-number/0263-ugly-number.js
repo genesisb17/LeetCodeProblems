d73b75053860ce16cda9fb5faefb9796aba3af39
Genesis Bonds <genesis.bonds@gmail.com>
2023-04-02 02:34:22 -0400

    Time: 62 ms (90.82%), Space: 43.5 MB (47.71%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.js
+++ 0263-ugly-number/0263-ugly-number.js
@@ -3,19 +3,22 @@
  * @return {boolean}
  */
 var isUgly = function(n) {
-    
-    while(n > 1){
-        if(n % 5 == 0){
-            n = n/5;
-        } else if(n % 3 == 0){
-            n = n/3;
-        } else if(n % 2 == 0){
-            n = n/2;
-        } else{
+        if(n === 0){
             return false;
         }
-    }
+  
+        while(n % 5 === 0){
+            n = n/5;
+        } 
+        while(n % 3 === 0){
+            n = n/3;
+        }
+        while(n % 2 === 0){
+            n = n/2;
+        } 
     
-    return n == 1;
+    
+         return n===1;
     
 };
+
