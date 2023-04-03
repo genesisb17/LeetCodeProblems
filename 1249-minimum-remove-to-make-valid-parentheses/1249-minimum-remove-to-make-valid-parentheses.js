6d1bdab23adad837427e4a97fad0de778a51f298
Genesis Bonds <genesis.bonds@gmail.com>
2023-04-03 16:17:05 -0400

    Time: 105 ms (21.68%), Space: 49.5 MB (36.13%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var minRemoveToMakeValid = function(s) {
+    
+    // set of invalid indecies
+    let invalid = new Set();
+    
+    // stack of parens
+    let brackets = [];
+    
+    for(let i = 0; i < s.length; i++){
+        if( s[i] == '(' ){
+            brackets.push([s[i], i]);
+        }
+        else if( s[i] == ')' ){
+            if(brackets.length == 0){
+                invalid.add(i);
+            }
+            else{
+            let curr = brackets.pop();
+            }
+              
+        }
+    }
+    if(brackets.length != 0){
+        for(let b of brackets){
+            invalid.add(b[1]);
+        }
+    }
+    
+    out = "";
+    for(let i = 0; i < s.length; i++){
+        if(!invalid.has(i)){
+            out+=s[i];
+        }
+    }
+    return out;
+    
+};
