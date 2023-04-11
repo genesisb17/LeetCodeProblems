9fb5b3c509633b8e896559f39300c64c15151612
Genesis Bonds <genesis.bonds@gmail.com>
2023-04-11 02:57:59 -0400

    Time: 74 ms (62.90%), Space: 46.7 MB (41.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -1,44 +1,4 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 
-function isPalindrome(str) {
-   let string = fixString(str);
-    console.log(string);
-  for(let i = 0; i < string.length/2; i++){
-    if(string[i] != string[string.length - 1 - i]){
-      return false;
-    }
-  }
-  return true;
-}
-
-
-let fixString = (string) => {
-    let out = "";
-    for(let s of string){
-        let code = s.charCodeAt(0);
-        if ((code > 47 && code < 58) || // numeric (0-9)
-            (code > 64 && code < 91) || // upper alpha (A-Z)
-            (code > 96 && code < 123)) { // lower alpha (a-z)
-                out += s.toLowerCase();
-            }
-    }
-    return out;
-}
-
-
-
-
-
-
-
-
-
-/**
-
-// old submission that seems much more complicated than necessary
 var isPalindrome = function(str) {
     let s = "";
     for(let ch of str ){
@@ -66,4 +26,3 @@ function isAlphanumeric(ch){
     }
     return true;
 }
-*/
