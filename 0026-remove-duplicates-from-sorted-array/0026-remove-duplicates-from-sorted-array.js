de9e75df4274af24d36478f25ee18d686a08ca2a
Genesis Bonds <genesis.bonds@gmail.com>
2023-04-12 14:40:33 -0400

    Time: 69 ms (80.75%), Space: 44.8 MB (52.81%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if(nums.legnth == 1) return nums.length;
+    
+    let slow = 0;
+    let fast = 1;
+    let total = nums.length;
+    let curr = 0;
+    while(fast <= total){
+        if(nums[fast] == nums[slow]){
+            fast++;
+        }
+        else{
+            nums[++slow] = nums[fast++];
+            curr++;
+        }
+    }
+    
+    return curr;
+    
+};
