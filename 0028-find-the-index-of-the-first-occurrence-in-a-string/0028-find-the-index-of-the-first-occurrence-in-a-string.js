af6f8031f72f1aa3f888ad89facf15eb359b2156
Genesis Bonds <genesis.bonds@gmail.com>
2023-04-17 19:56:14 -0400

    Time: 55 ms (77.42%), Space: 42.4 MB (20.02%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -5,15 +5,12 @@
  */
 var strStr = function(haystack, needle) {
     
-   // let slow = 0;
     let fast = 0;
     let index = -1;
     
     while(fast < haystack.length){
-        console.log(fast)
         if(haystack[fast] == needle[0]){
             index = fast;
-  //          slow = fast;
             let valid = true;
             for(let i = 1; i < needle.length; i++){
                 if(haystack[++fast] != needle[i]){
