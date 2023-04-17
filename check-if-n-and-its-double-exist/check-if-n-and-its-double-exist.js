888726d9243d0d0b261b85eac1fd683d35c1a988
Genesis Bonds <genesis.bonds@gmail.com>
2023-04-17 17:47:11 -0400

    Time: 77 ms, Memory: 44.3 MB - LeetHub

---
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function(arr) {
+    
+    arr.sort((a, b) => {
+        if(Math.abs(a) > Math.abs(b)) return 1;
+        if(Math.abs(a) < Math.abs(b)) return -1;
+        return 0;
+    })
+    
+    let check = new Set();
+    for(let num of arr){
+        if(check.has(num))return true;
+        check.add(num*2);
+    }
+    
+    return false;
+    
+};
