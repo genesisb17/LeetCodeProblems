51f787afcc7496f9e67cf0c56370d7e4ca6c5954
Genesis Bonds <genesis.bonds@gmail.com>
2023-04-19 02:13:02 -0400

    Time: 60 ms (88.56%), Space: 42.7 MB (98.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -1,5 +1,24 @@
 
+
 var majorityElement = function(nums) {
+    let candidate = nums[0];
+    let count = 1;
+    
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] == candidate) count++;
+        else{
+            if(--count == 0){
+                candidate = nums[i];
+                count++;
+            }
+        }
+    }
+    
+    return candidate;
+};
+
+
+var majorityElementSlow = function(nums) {
     
     nums.sort((a, b) => {
         if(a < b) return -1;
