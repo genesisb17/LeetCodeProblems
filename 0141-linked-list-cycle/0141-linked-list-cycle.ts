5f8672a6737ccca330cb4ee827db53c19a66b62a
Genesis Bonds <genesis.bonds@gmail.com>
2024-11-04 16:50:15 -0500

    Time: 55 ms (98.38%), Space: 53.5 MB (79.14%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    
+    
+    let slow = head;
+    let fast = head;
+    
+    while (true){
+      if(fast == null || fast.next == null || fast.next.next == null){
+        return false;
+      }
+      slow = slow.next;
+      fast = fast.next.next;
+      if(slow == fast){
+        return true;
+      }
+      
+    }
+    
+    return false;
+    
+};
