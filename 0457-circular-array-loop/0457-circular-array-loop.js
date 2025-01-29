b7eba2f84fcb203a02b6d62587f4ea07ca262a93
Genesis Bonds <genesis.bonds@gmail.com>
2025-01-28 21:20:55 -0500

    Time: 24 ms (61.29%), Space: 50 MB (38.71%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.js
+++ 0457-circular-array-loop/0457-circular-array-loop.js
@@ -1,39 +1,35 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
-var circularArrayLoop = function(nums) {
+function circularArrayLoop(nums){
+if(nums.length < 2){
+   return false;
+}
     
-  const len = nums.length;
-    
-  const nextIndex= function(current){
-    return ((current + nums[current]) % len + len) % len; 
-  }
-
-    if(nums.length < 2){
-      return false;
-    }
     for(let i = 0; i < nums.length; i++){
       let slow = i;
       let fast = i;
-      let pos = nums[i] > 0;
-      if(nextIndex(i) == i){
-        continue;
-      }
-      for(let j = 0; j < nums.length; j++){
-        fast = nextIndex(fast);
-        if(nums[slow] > 0 != pos || nums[fast] > 0 != pos){
-         break;
-        } 
-        fast = nextIndex(fast);
-        if(nums[fast] > 0 != pos){
-         break; 
+  
+  
+        while(true){
+        let nextSlow = nextIndex(nums, slow);
+        let firstFast = nextIndex(nums, fast);
+        let secondFast = nextIndex(nums, firstFast);
+        
+        if(!sameDirectionCheck(slow, fast, nextSlow, firstFast, secondFast, nums) || slow == nextSlow || firstFast == secondFast){
+          break;
         }
-         if(slow == fast){
-          return true;
+          slow = nextSlow;
+          fast = secondFast;
+          if(slow == fast){
+            return true;
+          }
         }
-      }
+      
     }
     return false;
-    
-};
+}
+function nextIndex(nums, index){
+  let n = nums.length;
+  return ((index + nums[index]) % n + n) % n;
+}
+function sameDirectionCheck(slow, fast, nextSlow, firstFast, secondFast, nums){
+   return nums[slow] * nums[nextSlow] > 0 && nums[firstFast] * nums[secondFast] > 0 ;
+}
